4672ca6d50e0489e2c2f9204aa41c203ca564459
Richard Atterfalk <richard@atterfalk.se>
2018-01-21 01:57:41 +0100

    Added gulp and trying out sass.

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const gulp      = require('gulp'),
+      minifyCSS = require('gulp-csso'),
+      rename    = require('gulp-rename'),
+      sass      = require('gulp-sass'),
+      uglify    = require('gulp-uglify');
+
+gulp.task('styles', ['styles:vendor'], function () {
+  return gulp.src(['./src/sass/**/*.scss', '!./src/sass/vendor/**/*.scss'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(minifyCSS())
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest('./static/css'));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('./src/scripts/**/*.js')
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./static/js'));
+});
+
+gulp.task('styles:vendor', ['styles:vendor:bootstrap', 'styles:vendor:font-awesome']);
+
+gulp.task('styles:vendor:bootstrap', function() {
+    return gulp.src('./node_modules/bootstrap/scss/**/*').pipe(gulp.dest('./styles/vendor/bootstrap'));
+});
+
+gulp.task('styles:vendor:font-awesome', function() {
+    return gulp.src('./node_modules/font-awesome/scss/**/*').pipe(gulp.dest('./styles/vendor/font-awesome'));
+});
+
+gulp.task('build', ['styles', 'scripts']);
