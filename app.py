b8dc4bed60f2e2433095de3079cb75b7038166be
Richard Atterfalk <richard@atterfalk.se>
2018-01-21 01:27:56 +0100

    Trying out the templates

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ FalloutApp = Flask(__name__)
 
 @FalloutApp.route("/")
 def index():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     FalloutApp.run()
