6b6e28040f83b0bcf046b67d7106b458aeac5f3d
Richard Atterfalk <richard@atterfalk.se>
2018-01-22 12:43:27 +0100

    Updated the code, in hope that it will not reload the page with the result.

--- app.py
+++ app.py
@@ -19,20 +19,21 @@ def calculators():
     return render_template('calculators.html', calculators=calc_list)
 
 
-@FalloutApp.route('/calculators/damage-resistance', methods=['GET', 'POST'])
+@FalloutApp.route('/calculators/damage-resistance', methods=['GET'])
 def calc_damage_resistance():
-    result = 0
-    if (request.method == 'POST'):
-        # damage = request.form['damage']
-        # damage_resist = request.form['damage_resist']
-
-        # dr = DamangeResistanceCalc()
-        # result = dr.damage_coeff(damage, damage_resist)
-        return jsonify(dict(damage_coeff=result))
-
     return render_template(
         'calculators/damage_resistance.html', damage_coeff=result
     )
+
+
+@FalloutApp.route('/api/calculators/damage-resistance', methods=['POST'])
+def api_calc_damage_resistance():
+    damage = request.form['damage']
+    damage_resist = request.form['damage_resist']
+
+    dr = DamangeResistanceCalc()
+    result = dr.damage_coeff(damage, damage_resist)
+    return jsonify(dict(damage_coeff=result))
 
 if __name__ == '__main__':
     FalloutApp.run()
