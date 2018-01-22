025c7f7625eaae536edfe85cd743d0625f0c7f0f
Richard Atterfalk <richard@atterfalk.se>
2018-01-22 12:51:26 +0100

    Updated the code, in hope that it will not reload the page with the result.

--- src/scripts/calculators/main.ts
+++ src/scripts/calculators/main.ts
@@ -1,19 +1,19 @@
 import axios, { AxiosRequestConfig, AxiosPromise } from 'axios';
 
 export function calculate_damage_coeff(e: any) {
-    e.preventDefault();
+  e.preventDefault();
 
-    axios({
-      method: 'post',
-      url: '/api/calculators/damage_resistance',
+  axios({
+    method: 'post',
+    url: '/api/calculators/damage_resistance',
 
-      data: {
-        damage: 100,
-        damage_resist: 50
-      }
-    }).then(function(response) {
-      console.log(response.data);
-    });
+    data: {
+      damage: 100,
+      damage_resist: 50
+    }
+  }).then(function(response) {
+    console.log(response.data);
+  });
 }
 
 var damage_coeff_form = <HTMLElement>document.body.querySelector('form');
