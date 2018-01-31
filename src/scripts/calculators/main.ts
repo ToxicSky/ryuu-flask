79e879b55d61e187196d6d6eb7057f3ec5ab5910
Richard Atterfalk <richard@atterfalk.se>
2018-01-31 20:54:23 +0100

    Working on frontend

--- src/scripts/calculators/main.ts
+++ src/scripts/calculators/main.ts
@@ -20,5 +20,7 @@ class Calculator
 }
 
 var calc = new Calculator;
+console.log(calc);
 var damage_coeff_form = document.querySelector('form');
 damage_coeff_form.addEventListener('onsubmit', calc.calculate_damage_coeff);
+console.log(damage_coeff_form);
