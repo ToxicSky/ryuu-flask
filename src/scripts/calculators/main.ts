381f2a3fb02ebc897f21c56212ef8fa088bb7ea0
Richard Atterfalk <richard@atterfalk.se>
2018-01-31 20:31:16 +0100

    Working on frontend

--- src/scripts/calculators/main.ts
+++ src/scripts/calculators/main.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig, AxiosPromise } from 'axios';
 
-export default class Calculator
+class Calculator
 {
   public calculate_damage_coeff(e: Event) {
     e.preventDefault();
