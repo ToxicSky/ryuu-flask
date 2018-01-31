7b01ae25a29757cbab0cba34f65bd8e9c7c221e8
Richard Atterfalk <richard@atterfalk.se>
2018-01-31 21:22:07 +0100

    Working on frontend

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,9 +1,10 @@
-import DamageCoeffCalculator from './calculators/damage_coeff_calculator';
+console.log('Hello, World!');
+// import DamageCoeffCalculator from './calculators/damage_coeff_calculator';
 
-(function() {
-    var calc = new DamageCoeffCalculator();
-    // console.log(calc);
-    var damage_coeff_form = document.querySelector('.damage_coeff_form');
-    console.log(damage_coeff_form);
-    damage_coeff_form.addEventListener('submit', calc.calculate_damage_coeff);
-})();
+// (function() {
+//     var calc = new DamageCoeffCalculator();
+//     // console.log(calc);
+//     var damage_coeff_form = document.querySelector('.damage_coeff_form');
+//     console.log(damage_coeff_form);
+//     damage_coeff_form.addEventListener('submit', calc.calculate_damage_coeff);
+// })();
